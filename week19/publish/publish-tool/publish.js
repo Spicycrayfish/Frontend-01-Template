e4598c6f27ab632f476b4924a849ab9021efea9d
谭轲 <your@email.com>
2020-08-20 00:29:42 +0800

    第十九周作业

--- week19/publish/publish-tool/publish.js
+++ week19/publish/publish-tool/publish.js
@@ -2,43 +2,53 @@ const http = require('http');
 const querystring = require('querystring');
 const fs = require('fs');
 
-const postData = querystring.stringify({
-  'content': 'Hello world, this is tank'
-});
+var archiver = require('archiver');
 
-let filename = './cat.jpg';
-fs.stat(filename, (err, stat) => {
-  console.log(stat.size);
+// const postData = querystring.stringify({
+//   'content': 'Hello world, this is tank'
+// });
 
+let packname = './package';
+// fs.stat(packname, (err, stat) => {
   const options = {
     host: 'localhost',
     port: 8081,
-    path: '/?filename=cat.jpg',
+    path: '/?filename=' + 'package.zip',
     method: 'POST',
     headers: {
-      'Content-Type': 'application/octet-stream',
-      'Content-Length': stat.size
+      'Content-Type': 'application/octet-stream'
     }
   };
-
 
   const req = http.request(options, (res) => {
     console.log(`STATUS: ${res.statusCode}`);
     console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
   });
 
-
   req.on('error', (e) => {
     console.error(`problem with request: ${e.message}`);
   });
 
-  let readStream = fs.createReadStream(filename);
-  readStream.pipe(req);
 
-  readStream.on('end', () => {
+  var archive = archiver('zip', {
+    zlib: { level: 9 } // Sets the compression level.
+  });
+
+  archive.directory(packname, false);
+  archive.finalize();
+  archive.pipe(req);
+
+  archive.on('end', () => {
     req.end();
   })
+
+  // let readStream = fs.createReadStream(packname);
+  // readStream.pipe(req);
+  // readStream.on('end', () => {
+  //   req.end();
+  // })
+
   // req.write(postData);
-});
+// });
 
 
